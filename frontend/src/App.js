e065fcc1a2eca708c58dcac14e40e56f8bb558aa
James Jackson <jmjb318@gmail.com>
2026-08-24 23:09:20 -0700

    feat(frontend): add error boundary with recovery UI

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,19 +3,22 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import ListingsPage from './pages/ListingsPage';
 import PropertyDetailPage from './pages/PropertyDetailPage';
 import NaturalSearchPage from './pages/NaturalSearchPage';
+import ErrorBoundary from './components/ErrorBoundary';
 import './App.css';
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="App">
-        <Routes>
-          <Route path="/" element={<ListingsPage />} />
-          <Route path="/property/:id" element={<PropertyDetailPage />} />
-          <Route path="/search/natural" element={<NaturalSearchPage />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
+    <ErrorBoundary>
+      <BrowserRouter>
+        <div className="App">
+          <Routes>
+            <Route path="/" element={<ListingsPage />} />
+            <Route path="/property/:id" element={<PropertyDetailPage />} />
+            <Route path="/search/natural" element={<NaturalSearchPage />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </ErrorBoundary>
   );
 }
 
